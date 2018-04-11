41c68f6ba78cba994f836934770ed6694c821f1d
DrNate <nathan.descombes@epsi.fr>
2018-04-11 15:05:59 +0200

    Changement php

--- workshop/src/gestionProjet.php
+++ workshop/src/gestionProjet.php
@@ -2,16 +2,17 @@
 <html>
 <head>
     <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Home</title>
+    <title>Gestion projet</title>
     <link rel="icon" type="image/png" href="images/logo.png" />
     <link rel="stylesheet" href="styled.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.6.2/css/bulma.min.css">
     <script defer src="https://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>
 </head>
 <body>
-    <button class="button 0" id="deconnexion">Se déconnecter</button>
-    <button class="button 1" id="creer-projet">Créer projet</button>
-    <button class="button 1" id="gerer-projet">Gérer les projets</button>
+    <div>
+        <?php
+        
+        ?>
+    </div>
 </body>
 </html>
