23afc1bad06170b3be9bab8d7e30b01e117bf558
Rascarat <ttheo38@gmail.com>
2018-04-11 16:52:50 +0200

    Add function in requetes

--- workshop/src/requetes.php
+++ workshop/src/requetes.php
@@ -7,7 +7,25 @@
 			while($donnee = $p_requete->fetch()) {
             $mail = $donnee["Mail"];
             $mdp = $donnee["mot_de_passe"];
-            }
+            } 
+            $p_requete->closeCursor();
+    }
+
+    function getConnexionPed() {
+        $p_requete = $p_base->query('SELECT Mail, mot_de_passe FROM responsable');
+			while($donnee = $p_requete->fetch()) {
+            $mail = $donnee["Mail"];
+            $mdp = $donnee["mot_de_passe"];
+            } 
+            $p_requete->closeCursor();
+    }
+
+    function getConnexionInt() {
+        $p_requete = $p_base->query('SELECT Mail, mot_de_passe FROM intervenant');
+			while($donnee = $p_requete->fetch()) {
+            $mail = $donnee["Mail"];
+            $mdp = $donnee["mot_de_passe"];
+            } 
             $p_requete->closeCursor();
     }
 ?>
